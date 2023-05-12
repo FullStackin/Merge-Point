342c767daf6e2070bca46075385bc4f7714e85e1
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-12 15:54:09 -0700

    fix: rename script to correct name

--- backend/psql-setup-script.js
+++ backend/psql-setup-script.js
@@ -1,4 +1,4 @@
-const { sequelize } = require('./db/models'); // Importing the "sequelize" object from the "models.js" file in the "db" folder.
+const { sequelize } = require("./db/models"); // Importing the "sequelize" object from the "models.js" file in the "db" folder.
 
 // Calling the "showAllSchemas" method on the "sequelize" object with an options object that turns off logging.
 // This method will return a promise that resolves to an array of all the schema names in the database.
