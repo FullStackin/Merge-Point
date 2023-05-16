0eed710c423292c5a348aca813b72f4853812cf7
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-16 13:47:19 -0700

    fix: redo migration files

---
+++ backend/db/migrations/20230516203106-create-event.js
@@ -0,0 +1,71 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable(
+      "Events",
+      {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        venueId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Venues",
+          },
+        },
+        groupId: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: "Groups",
+          },
+        },
+        name: {
+          type: Sequelize.STRING,
+        },
+        description: {
+          type: Sequelize.TEXT,
+        },
+        type: {
+          type: Sequelize.ENUM("Online", "In person"),
+        },
+        capacity: {
+          type: Sequelize.INTEGER,
+        },
+        price: {
+          type: Sequelize.INTEGER,
+        },
+        startDate: {
+          type: Sequelize.DATE,
+        },
+        endDate: {
+          type: Sequelize.DATE,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+          defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        },
+      },
+      options
+    );
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Events";
+    await queryInterface.dropTable(options);
+  },
+};
