ce5366a22177fa549a01f040f0dba555addfa0c8
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-16 13:08:15 -0700

    style: remove import and use file directly

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,8 +1,6 @@
-const config = require("./index");
-
 module.exports = {
   development: {
-    storage: config.dbFile,
+    storage: process.env.DB_FILE,
     dialect: "sqlite",
     seederStorage: "sequelize",
     logQueryParameters: true,
