33f7f47b1cc7dd6c0a3b7f01861bf704116b6e81
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-05-20 16:44:57 -0700

    fix: adjust code in group.js file

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -15,9 +15,43 @@ const { requireAuth } = require("../../utils/auth.js");
 const { handleValidationErrors } = require("../../utils/validation.js");
 
 router.get("/", async (req, res) => {
-  const groups = await Group.findAll();
+  const groups = await Group.findAll({
+    include: {
+      model: GroupImage,
+      where: {
+        preview: true,
+      },
+      attributes: ["url"],
+      required: false,
+    },
+  });
 
-  res.json(groups);
+  const newGroups = await Promise.all(
+    groups.map(async (group) => {
+      const groupJson = group.toJSON();
+      const numMembers = await Membership.count({
+        where: {
+          groupId: groupJson.id,
+          status: {
+            [Op.in]: ["co-host", "member"],
+          },
+        },
+      });
+
+      const { url } = groupJson.GroupImages[0]
+        ? groupJson.GroupImages[0]
+        : { url: null };
+
+      return {
+        ...groupJson,
+        numMembers,
+        previewImage: url,
+        GroupImages: undefined,
+      };
+    })
+  );
+
+  res.json({ Groups: newGroups });
 });
 
 router.get("/current", requireAuth, async (req, res) => {
