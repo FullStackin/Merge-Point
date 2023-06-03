9428881d23a97c51ee05c66ea120d202338788bb
Omar El Sahlah <98979833+FullStackin@users.noreply.github.com>
2023-06-02 17:45:29 -0700

    feat: adjusted comments

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -9,9 +9,6 @@ const groupImagesRouter = require("./groupImages.js");
 const eventImagesRouter = require("./eventImages.js");
 const { restoreUser } = require("../../utils/auth.js");
 
-// Connect restoreUser middleware to the API router
-// If current user session is valid, set req.user to the user in the database
-// If current user session is not valid, set req.user to null
 
 router.use(restoreUser);
 
